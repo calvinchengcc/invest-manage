3e912e2ff77493f3275ba91350bcd17d9a5629f1
Calvin Cheng <calvin.cheng.cc@outlook.com>
2016-03-11 01:23:01 -0800

    Add ids to seed data and refactor seeds.rb

--- project-final/db/seeds.rb
+++ project-final/db/seeds.rb
@@ -11,22 +11,22 @@ def main
   user = CreateAdminService.new.call
   puts 'CREATED ADMIN USER: ' << user.email
 
-  put_csv_data_in_record('exchanges', Exchange)
+  seed_record('exchanges', Exchange)
   puts 'ADDED STOCK EXCHANGES'
 
-  put_csv_data_in_record('stocks', Stock)
+  seed_record('stocks', Stock)
   puts 'ADDED STOCKS'
 
-  put_csv_data_in_record('addresses', Address)
+  seed_record('addresses', Address)
   puts 'ADDED ADDRESSES'
 
-  put_user_data('users')
+  seed_record('users', User)
   puts 'ADDED USERS'
 
-  put_csv_data_in_record('portfolios', Portfolio)
+  seed_record('portfolios', Portfolio)
   puts 'ADDED PORTFOLIOS'
 
-  put_csv_data_in_record('holdings', Holding)
+  seed_record('holdings', Holding)
   puts 'ADDED HOLDINGS'
 end
 
@@ -35,21 +35,10 @@ def parse_csv_file(filename)
   SmarterCSV.process file
 end
 
-def put_csv_data_in_record(filename, record)
+def seed_record(filename, record)
   data = parse_csv_file(filename)
   ActiveRecord::Base.transaction do
-    data.each do |row|
-      record.find_or_create_by! row
-    end
-  end
-end
-
-def put_user_data(filename)
-  data = parse_csv_file filename
-  ActiveRecord::Base.transaction do
-    data.each do |row|
-      User.create(row) if User.find_by(email: row[:email]).nil?
-    end
+    data.each { |row| record.create!(row) if record.find_by(id: row[:id]).nil? }
   end
 end
 
