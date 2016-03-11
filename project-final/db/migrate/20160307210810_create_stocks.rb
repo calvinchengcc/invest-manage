81d4946773562738814070da1ec6055ec4bc71c2
Calvin Cheng <calvin.cheng.cc@outlook.com>
2016-03-10 21:32:23 -0800

    Fix referential integrity issue in PostgreSQL

--- project-final/db/migrate/20160307210810_create_stocks.rb
+++ project-final/db/migrate/20160307210810_create_stocks.rb
@@ -2,7 +2,7 @@ class CreateStocks < ActiveRecord::Migration
   def change
     create_table :stocks do |t|
       t.string :symbol, null: false
-      t.string :exchange_id, null: false
+      t.integer :exchange_id, null: false
       t.string :name, null: false
 
       t.index :symbol, unique: true
