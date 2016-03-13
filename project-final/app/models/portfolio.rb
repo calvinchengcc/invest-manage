4c37901d003400c02e08dd89c5c8f543f34a3abe
Calvin Cheng <calvin.cheng.cc@outlook.com>
2016-03-13 01:44:35 -0800

    Add methods to get current price and return info from Portfolio

--- project-final/app/models/portfolio.rb
+++ project-final/app/models/portfolio.rb
@@ -2,4 +2,35 @@ class Portfolio < ActiveRecord::Base
   has_many :holdings, dependent: :restrict_with_error
   belongs_to :owner, class_name: 'User'
   belongs_to :manager, class_name: 'User'
+
+  def current_value
+    cash + current_value_of_holdings
+  end
+
+  def current_value_of_holdings
+    symbols = self.holdings.map(&:stock).map(&:symbol)
+    return 0 if symbols.empty?
+    Array.wrap(StockQuote::Stock.quote(symbols))
+        .map(&:last_trade_price_only)
+        .zip(self.holdings.map(&:num_shares))
+        .map { |a| a[0] * a[1] }.reduce(:+)
+  end
+
+  def profit_loss
+    current_value - principal
+  end
+
+  def annualized_return
+    (current_value / principal) ** (1 / years_since(creation_date)) - 1 * 100
+  end
+
+  def total_return
+    (current_value / principal - 1) * 100
+  end
+
+  private
+
+  def years_since(date)
+    (Date::today.to_time - date.to_time) / 1.year.seconds
+  end
 end
--- project-final/app/models/stock.rb
+++ project-final/app/models/stock.rb
@@ -2,19 +2,26 @@ class Stock < ActiveRecord::Base
   belongs_to :exchange
   has_many :holdings
 
+  before_validation :lookup_name_and_exchange_if_needed
   validate :listed_on_us_exchange
+  validates :symbol, :exchange_id, :name, presence: true
 
-  before_validation :lookup_exchange_and_name_if_not_given
-
-  def listed_on_us_exchange
-    unless Exchange.exists?(self.exchange_id)
-      errors.add(self.symbol, 'is not listed on a US stock exchange.')
+  def lookup_name_and_exchange_if_needed
+    if name.nil? || exchange.nil?
+      query = StockQuote::Stock.quote(self.symbol)
+      self.exchange ||= Exchange.find_by(code: query.stock_exchange)
+      self.name ||= query.name
     end
   end
 
-  def lookup_exchange_and_name_if_not_given
-    query = StockQuote::Stock.quote(self.symbol)
-    self.exchange_id ||= Exchange.find_by!(code: query.stock_exchange).id
-    self.name ||= query.name
+  def listed_on_us_exchange
+    unless Exchange.exists?(exchange_id)
+      errors.add(symbol, 'is not listed on a US stock exchange.')
+    end
   end
+
+  def current_price
+    StockQuote::Stock.quote(symbol).last_trade_price_only
+  end
+
 end
